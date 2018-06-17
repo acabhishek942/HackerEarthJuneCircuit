0399874efb835ae0d6efe6e2be405cde3d410186
Abhishek Choudhary <acabhishek942@gmail.com>
2018-06-17 18:33:54 +0530

    need to figure out how to arrane using lexical order

--- find_the_subset.py
+++ find_the_subset.py
@@ -11,6 +11,22 @@ def calculate_ugliness(T, D):
             ugliness += 1
     return ugliness
 
+def get_lowest_lexical_order(set1, set2):
+    pass
+
 N, M, D = (int(i) for i in input().split())
 T = (int(i) for i in input().split())
-print (T)
+
+min_set = ()
+min_ugliness = 100000
+for i in itertools.combinations(T, N):
+    current_ugliness = calculate_ugliness(i, D)
+    if current_ugliness <= min_ugliness:
+        if current_ugliness == min_ugliness:
+            min_set = get_lowest_lexical_order(i, previous_min_ugliness)
+        min_ugliness = current_ugliness
+        previous_min_ugliness_set = i
+
+print(calculate_ugliness((1, 4, 3, 2), 2))
+print(calculate_ugliness((1, 4, 5, 3), 2))
+# print(max_set)
